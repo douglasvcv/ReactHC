0077f352645bf0f0ca6e4bc2c62154d66e68b100
Douglas <douglasvitor1233@gmail.com>
2024-03-03 02:06:25 -0300

    learning more about hooks

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,18 +1,36 @@
-function Form(){
+import {useState} from 'react'
 
+function Form(){
 function cadastrarUsuario(e){
     e.preventDefault()
-    console.log("Cadastrou usuário")
+    console.log(`Usuário: ${name}
+    Senha: ${password}`)
 }
+
+    const [name, setName] = useState("Douglas")
+    const [password, setPassword] = useState("Douglas")
 
     return (
         <>
         <h1>Meu cadastro</h1>
         <form onSubmit={cadastrarUsuario}>
             <div>
-                <input type="text" placeholder="Digite seu nome"/>
+                <label htmlFor="name">Nome: </label>
+                <input type="text"
+                id="name"
+                name="name"
+                placeholder="Digite seu nome"
+                onChange={(e) => {setName(e.target.value)}}
+                />
+                <label htmlFor="password">Senha: </label>
+                <input type="password"
+                 id="password"
+                 name="password"
+                 placeholder="Digite sua senha"
+                 onChange={(e) => {setPassword(e.target.value)}}
+                 />
             </div>
-            <div>
+            <div> 
                 <input type="submit" value="Cadastrar"/>
                 
             </div>
