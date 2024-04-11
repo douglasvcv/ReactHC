c2ad98373aacb4e2754f840e98e057ce9e0c6f6e
Douglas <douglasvitor1233@gmail.com>
2024-04-11 20:58:36 -0300

    Learning about react router

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,29 +1,24 @@
-import HelloWorld from './components/HelloWorld';
-import Frase from './components/Frase';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import List from './components/List';
-import Evento from './components/Evento';
-import Form from './components/Form';
-import Condicional from './components/Condicional';
-import OutraLista from './components/OutroEvento';
-import SeuNome from './components/SeuNome';
-import { useState } from 'react';
-import Saudacao from './components/Saudacao';
+import Home from './pages/Home';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
 
-  const nome = "Levi"
-  const array = ['Douglas', 'Lima', 'Geovani', 'Risort']
-  const [name, setName] = useState()
+  
   return (
-    <div className="App">
-      <OutraLista itens={array}></OutraLista>
-      <SeuNome setName={setName}></SeuNome>
-      <p>{name}</p>
-      <Saudacao name={name} ></Saudacao>
-    </div>
+    <Router>
+      <NavBar></NavBar>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Empresa></Empresa>}/>
+        <Route path='/contato' element={<Contato></Contato>}/>
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
---
+++ meu-projeto/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <>
+        <footer>Feito com amor por Douglas</footer>
+        </>
+    )
+}
+
+export default Footer
