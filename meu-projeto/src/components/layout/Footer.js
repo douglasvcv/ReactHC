b56031041aaff14f7d8dcd28fdca36966b9aafdc
Douglas <douglasvitor1233@gmail.com>
2024-04-11 21:53:45 -0300

    react icons package

--- meu-projeto/src/components/layout/Footer.js
+++ meu-projeto/src/components/layout/Footer.js
@@ -1,7 +1,16 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './footer.module.css'
+
 function Footer(){
     return(
         <>
-        <footer>Feito com amor por Douglas</footer>
+        <footer>
+            <ul className={styles.list}>
+                <li className={styles.item}><FaFacebook></FaFacebook></li>
+                <li className={styles.item}><FaInstagram></FaInstagram></li>
+                <li className={styles.item}><FaLinkedin></FaLinkedin></li>
+            </ul>
+        </footer>
         </>
     )
 }
